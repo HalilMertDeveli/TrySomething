6af8d109ec78a64ec859d029303c825f72f93bea
haliko <halilmertdeveliii@gmail.com>
2023-07-16 00:56:38 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:LoginPage()
+  ));
+}
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
