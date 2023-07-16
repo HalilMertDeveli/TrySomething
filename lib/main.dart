a63a09c8e8365d7da7044e761d3499d3964e3a4c
haliko <halilmertdeveliii@gmail.com>
2023-07-16 14:07:59 +0300

    other stream usages with stream controller

--- lib/main.dart
+++ lib/main.dart
@@ -6,58 +6,60 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: HomePage(),
     ),
   );
 }
 
-class LoginPage extends StatefulWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  int number = 0;
-
-  Stream<int> createStream() {
-    return Stream.periodic(
-      Duration(seconds: 2),
-      (count) => number = (count * 2),
-    );
-  }
-  
-  @override
-  void initState() {
-    super.initState();
-    createStream().listen((eventReturn) {
-      setState(() {
-        number = eventReturn;
-      });
-     });
-     
-  }
-
- 
-
+class _HomePageState extends State<HomePage> {
+  final counterBlock = CounterBlock();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder<int>(
-        stream:createStream(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListTile(
-              title: Text("Our value is = ${number.toString()}"),
-            );
-          }
-          if (snapshot.hasError) {
-            return Text("There is error in builder things");
-          }
-          else{
-            return CircularProgressIndicator();
-          }
-        },
+      appBar: AppBar(
+        title: const Text(
+          "Stream usages",
+          textAlign: TextAlign.center,
+        ),
       ),
+      body: Center(
+          child: StreamBuilder<int>(
+            stream: counterBlock.counterStream,
+            initialData: 0,
+            builder: (context, snapshot) {
+              return Text(
+                'Counter: ${snapshot.data}',
+                style: TextStyle(fontSize: 24),
+              );
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            counterBlock.incrementCounter();
+          },
+          child: Icon(Icons.add),
+        ),
+      
     );
+  }
+}
+
+class CounterBlock {
+  final _counterController = StreamController<int>();
+  Stream<int> get counterStream => _counterController.stream;
+
+  int _counter = 0;
+
+  void incrementCounter() {
+    _counter++;
+    _counterController.sink.add(_counter);
   }
 }
