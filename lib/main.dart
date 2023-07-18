4dfc7dd2b6602e48411d8b8c1f4d350ee8fab3dc
haliko <halilmertdeveliii@gmail.com>
2023-07-18 18:42:59 +0300

    data communication

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,50 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: HomePage(
+        userName: "Halil Mert",
+        userSurName: "Develi",
+      ),
     ),
   );
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  final String userName;
+  final String userSurName;
+  const HomePage(
+      {super.key, required this.userName, required this.userSurName});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  String id = "";
-  String name = "";
-
-  String stringSubOperation(String inputString) {
-    return inputString.substring(0, inputString.indexOf("_"));//0 'dan başla _ görene kadar devam et, _ gördükten sonra kalan kısmı sil at
-  }
-
-  String nameSubStringOperation(String inputName) {
-    //return inputName.substring(inputName.indexOf("_")+1);//_ gördükten sonra bir eleman olan kendisini atla ve yazdır
-    //return inputName.substring(inputName.indexOf("_")+3);//_ gördükten sonra iki eleman olan ve  kendisi de dahil  atla ve yazdır
-    return inputName.substring(inputName.indexOf("_"),3);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    id = stringSubOperation("halil_mert");
-    name = nameSubStringOperation("_Halil");
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("String manipulation"),
+        title: const Text(
+          "Some class operation",
+          style: TextStyle(
+            color: Colors.black12,
+            fontWeight: FontWeight.bold,
+            fontSize: 23,
+          ),
+          textAlign: TextAlign.center,
+        ),
       ),
       body: Column(
         children: [
           Center(
-            child: Text(
-              id + " = " + name,
-            ),
+            child: Text("userName: ${widget.userName}"),
           ),
+          SizedBox(
+            height: 22,
+          ),
+          Text("user sur name: ${widget.userSurName}"),
         ],
       ),
     );
