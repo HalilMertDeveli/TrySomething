5e03492154a2be04f1959a1b969e65aeebc052d8
haliko <halilmertdeveliii@gmail.com>
2023-07-21 12:00:22 +0300

    carrying data page to another without state Manangment

--- lib/main.dart
+++ lib/main.dart
@@ -3,66 +3,72 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: HomePage(
-        userName: "Halil Mert",
-        userSurName: "Develi",
-      ),
+      home: HomePage(),
     ),
   );
 }
 
-// 
-
 class HomePage extends StatefulWidget {
-  final String userName;
-  final String userSurName;
-  const HomePage(
-      {super.key, required this.userName, required this.userSurName});
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final elementForList = ["element 1", "element 2", "element 3"];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Sorting element in Listview builder",
-          style: TextStyle(
-              color: Colors.amber, fontSize: 33, fontWeight: FontWeight.bold),
+        title: const Text("Home page"),
+      ),
+      body: Center(
+        child: IconButton(
+          onPressed: () async {
+            await goLoginPage(
+              context,
+              Loginpage(
+                  userName: "Halil Mert", userSurName: "Develi", userAge: 13),
+            );
+          },
+          icon: Icon(Icons.next_plan),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount:elementForList.length,
-              itemBuilder: (BuildContext context, int index) {
-                //int reversedIndex = elementForList.length-index -1;
-              return ListTile(
-                title: Text(
-                  elementForList[index].toString(),
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                ),
-                subtitle: Text(
-                  index.toString(),
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22,
-                  ),
-                ),
-              );
-            }),
-          ),
-        ],
+    );
+  }
+}
+
+Future<void> goLoginPage(BuildContext context, Widget className) async {
+  await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => className,
+      ));
+}
+
+class Loginpage extends StatefulWidget {
+  final String userName;
+  final String userSurName;
+  final int userAge;
+  const Loginpage(
+      {super.key,
+      required this.userName,
+      required this.userSurName,
+      required this.userAge});
+
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Page"),
+      ),
+      body: Center(
+        child: Text("Login page "),
       ),
     );
   }
