e2bc1fa46cfe5eecf489e9b653a188431e8eb84f
haliko <halilmertdeveliii@gmail.com>
2023-07-21 13:00:23 +0300

    i added everything about firebase then i will write the working code next time i come

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:try_something/firebase_options.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     ),
   );
@@ -19,57 +26,8 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home page"),
-      ),
-      body: Center(
-        child: IconButton(
-          onPressed: () async {
-            await goLoginPage(
-              context,
-              Loginpage(
-                  userName: "Halil Mert", userSurName: "Develi", userAge: 13),
-            );
-          },
-          icon: Icon(Icons.next_plan),
-        ),
-      ),
+      appBar: AppBar(),
     );
   }
 }
 
-Future<void> goLoginPage(BuildContext context, Widget className) async {
-  await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => className,
-      ));
-}
-
-class Loginpage extends StatefulWidget {
-  final String userName;
-  final String userSurName;
-  final int userAge;
-  const Loginpage(
-      {super.key,
-      required this.userName,
-      required this.userSurName,
-      required this.userAge});
-
-  @override
-  State<Loginpage> createState() => _LoginpageState();
-}
-
-class _LoginpageState extends State<Loginpage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login Page"),
-      ),
-      body: Center(
-        child: Text("Login page "),
-      ),
-    );
-  }
-}
