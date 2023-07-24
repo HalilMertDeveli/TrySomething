5ceca0a8e1b1a48f9c144637f40f2c9a8bd18df2
haliko <halilmertdeveliii@gmail.com>
2023-07-24 14:26:05 +0300

    evrything is set next time you come continue with sharedPreferencesse for login state

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -19,35 +19,14 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(49)),
-              focusedBorder:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(49)),
-              errorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(49),
-              ),
-              label: Text(
-                "Enter your Email ",
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
             ),
-            onChanged: (value) {
-              setState(() {
-                userEmail = value;
-              });
-            },
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            decoration: InputDecoration(
+            TextField(
+              decoration: InputDecoration(
                 border:
                     OutlineInputBorder(borderRadius: BorderRadius.circular(49)),
                 focusedBorder:
@@ -55,61 +34,84 @@ class _LoginPageState extends State<LoginPage> {
                 errorBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(49),
                 ),
-                label: Text("Enter you userName")),
-            onChanged: (value) {
-              setState(() {
-                userName = value;
-              });
-            },
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(49)),
-                focusedBorder:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(49)),
-                errorBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(49),
+                label: Text(
+                  "Enter your Email ",
                 ),
-                label: Text("Enter your password")),
-            onChanged: (value) {
-              setState(() {
-                userPassword = value;
-              });
-            },
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              if (userName != "" && userEmail != "" && userPassword != "") {
-                await firebaseAuthService
-                    .registerWithEmailAndPassword(
-                        userName, userEmail, userPassword)
-                    .then(
-                      (value) => goToGivedScreen(
-                        context,
-                        WelcomePage(userName: userName, userEmail: userEmail),
-                      ),
-                    );
-              }
-            },
-            child: Row(
-              children: [
-                Icon(Icons.create),
-                Text(
-                  "Create group with firebase,",
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+              ),
+              onChanged: (value) {
+                setState(() {
+                  userEmail = value;
+                });
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  border:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(49)),
+                  focusedBorder:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(49)),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(49),
                   ),
-                ),
-              ],
+                  label: Text("Enter you userName")),
+              onChanged: (value) {
+                setState(() {
+                  userName = value;
+                });
+              },
             ),
-          ),
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  border:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(49)),
+                  focusedBorder:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(49)),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(49),
+                  ),
+                  label: Text("Enter your password")),
+              onChanged: (value) {
+                setState(() {
+                  userPassword = value;
+                });
+              },
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (userName != "" && userEmail != "" && userPassword != "") {
+                  await firebaseAuthService
+                      .registerWithEmailAndPassword(
+                          userName, userEmail, userPassword)
+                      .then(
+                        (value) => goToGivedScreen(
+                          context,
+                          WelcomePage(userName: userName, userEmail: userEmail),
+                        ),
+                      );
+                }
+              },
+              child: Row(
+                children: [
+                  Icon(Icons.create),
+                  Text(
+                    "Create group with firebase,",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
